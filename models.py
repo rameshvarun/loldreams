3969af91421d35b93b62e6ee2579301d6e1202ff
Varun Ramesh <varun.ramesh@aol.com>
2013-12-14 18:54:19 -0700

    basic models

---
+++ models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Champion model
+class Champion(models.Model):
+	name = models.CharField(max_length=200)
+	
+	def __unicode__(self):
+		return self.name
+
+# Store game information
+class Game(models.Model):
+	riotid = models.IntegerField() #Id assigned by Riot in the API
+	tier = models.IntegerField() #Store the tier level of the game
+	
+	team1 = models.ManyToManyField(Champion, related_name='team1') #Champions of team 1
+	team2 = models.ManyToManyField(Champion, related_name='team2') #Champions of team 2
+	
+	result = models.BooleanField() #True if team 1 won, false if team 2 won
