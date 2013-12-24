9abc9183c3016ac20522616ef78d96ea72ac3e6c
Varun Ramesh <varun.ramesh@aol.com>
2013-12-24 00:08:14 -0700

    Added command deleteallgames

---
+++ management/commands/deleteallgames.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from loldreams.models import *
+
+
+class Command(BaseCommand):
+	help = 'Deletes all games in the database.'
+	
+	def handle(self, *args, **options):
+		games = Game.objects.all()
+		self.stdout.write( str(len(games)) + " games in database deleted.")
+		games.delete()
