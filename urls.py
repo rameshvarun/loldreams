7beaaa67cb2a9969a109215eb1eaaee3af771874
Varun Ramesh <varun.ramesh@aol.com>
2013-12-27 14:02:12 -0500

    Reccomendations are now performed on the server, instead of the client.

--- urls.py
+++ urls.py
@@ -4,5 +4,6 @@ from loldreams import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.home, name='home'),
-	url(r'^win_rate', views.win_rate, name='win_rate')
+	url(r'^win_rate', views.win_rate, name='win_rate'),
+	url(r'^reccomendations', views.reccomendations, name='reccomendations')
 )
--- views.py
+++ views.py
@@ -6,6 +6,9 @@ from loldreams.models import *
 import json
 import time
 
+def jsonResponse(jsonDict):
+	return HttpResponse(json.dumps(jsonDict, indent=4), content_type="application/json")
+
 def home(request):
 	context = {
 		"champions" : Champion.objects.all()
@@ -53,4 +56,49 @@ def win_rate(request):
 	if sample_size > 0:
 		response['winrate'] = (float(wins)/(wins + losses))
 		
-	return HttpResponse(json.dumps(response, indent=4), content_type="application/json")
+	return jsonResponse(response)
+	
+SMALLEST_SAMPLE_SIZE = 5
+	
+def reccomendations(request):
+	#Benchmarking
+	start = time.clock()
+	
+	#Need to have two queries, as the specific champion combination might match either team1 or team2
+	team1_games = Game.objects.filter(region = request.GET['region'] )
+	team2_games = Game.objects.filter(region = request.GET['region'] )
+	
+	#Filter down by champion id
+	for champion_id in request.GET.getlist('id'):
+		team1_games = team1_games.filter(team1__riotid = champion_id )
+		team2_games = team2_games.filter(team2__riotid = champion_id )
+		
+	if len(team1_games) + len(team2_games) < SMALLEST_SAMPLE_SIZE:
+		return jsonResponse({ 'error' : 'This champion does not have a large enough sample size.'})
+	
+	response = {
+		'champions' : []
+	}
+	
+	for champion in Champion.objects.all():
+		if str(champion.riotid) not in request.GET.getlist('id'):
+			result = {}
+			possible_team1 = team1_games.filter(team1 = champion)
+			possible_team2 = team2_games.filter(team2 = champion)
+			
+			sample_size = len(possible_team1) + len(possible_team2)
+			
+			if sample_size >= SMALLEST_SAMPLE_SIZE:
+				possible_team1 = possible_team1.filter(result=True)
+				possible_team2 = possible_team2.filter(result=False)
+				
+				wins = len(possible_team1) + len(possible_team2)
+				
+				result['name'] = champion.name
+				result['winrate'] = float(wins)/sample_size
+				result['sample_size'] = sample_size
+				
+				response['champions'].append(result)
+	
+	response['time'] = time.clock() - start
+	return jsonResponse(response)
